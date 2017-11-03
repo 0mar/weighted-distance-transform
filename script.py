72fc9d283210ee031810fe6645e42aee3834ab41
0mar <omsrichardson@gmail.com>
2017-11-03 16:31:31 +0100

    Implemented WDT

---
+++ script.py
@@ -0,0 +1,153 @@
+import numpy as np
+import math
+import heapq
+from scipy.misc import imread
+
+import operator
+
+DIR_STRINGS = ["left", "up", "right", "down"]
+AXES = ['x', 'y']
+DIRS = np.array([[-1, 0], [0, 1], [1, 0], [0, -1]])
+
+
+def read_image(filename='ex1.png'):
+    data = imread(filename, mode='RGB')
+    exits = np.where(data[:, :, 0] - data[:, :, 1] > 200)
+    obstacles = np.where(data[:, :, 1] < 100)
+    u = np.ones(data[:, :, 0].shape)
+    u[obstacles] = np.inf
+    u[exits] = 0
+    return u
+
+
+# TODO: Change "potentials" to "distances"
+def exists(index):
+    """
+    Checks whether an index exists an array
+    :param index: 2D index tuple
+    :param max_index: max index tuple
+    :return: true if lower than tuple, false otherwise
+    """
+    return (0 <= index[0] < nx) and (0 <= index[1] < ny)
+
+
+def get_new_candidate_cells(new_known_cells, unknown_cells):
+    new_candidate_cells = set()
+    for cell in new_known_cells:
+        for direction in DIRS:
+            nb_cell = (cell[0] + direction[0], cell[1] + direction[1])
+            if nb_cell in unknown_cells:
+                new_candidate_cells.add(nb_cell)
+    return new_candidate_cells
+
+
+def compute_potential(cell, costs, potential):
+    # Find the minimal directions along a grid cell.
+    # Assume left and below are best, then overwrite with right and up if they are better
+    adjacent_potentials = np.ones(4) * np.inf
+    pots_from_axis = [0, 0]  # [x direction, y direction]
+    costs_from_axis = [np.inf, np.inf]  #
+    for i, dir_s in enumerate(DIR_STRINGS):
+        # Direction for which we check the cost
+        normal = DIRS[i]
+        nb_cell = (cell[0] + normal[0], cell[1] + normal[1])
+        if not exists(nb_cell):
+            continue
+        pot = potential[nb_cell]
+        # potential in that neighbour field
+        if dir_s == 'left':
+            face_index = (nb_cell[0] + 1, nb_cell[1])
+        elif dir_s == 'down':
+            face_index = (nb_cell[0], nb_cell[1] + 1)
+        else:
+            face_index = nb_cell
+        # Left/right is x, up/down is y
+        cost = costs[i % 2][face_index]
+        # Proposed cost along this direction
+        adjacent_potentials[i] = pot + cost
+        # If it is cheaper to go from the opposite direction
+        if adjacent_potentials[i] < adjacent_potentials[(i + 2) % 4]:
+            pots_from_axis[i % 2] = pot
+            costs_from_axis[i % 2] = cost
+        hor_pot, ver_pot = pots_from_axis
+        hor_cost, ver_cost = costs_from_axis
+        # Coefficients of quadratic equation (upwind discretization)
+    a = 1 / hor_cost ** 2 + 1 / ver_cost ** 2
+    b = -2 * (hor_pot / hor_cost ** 2 + ver_pot / ver_cost ** 2)
+    c = (hor_pot / hor_cost) ** 2 + (ver_pot / ver_cost) ** 2 - 1
+
+    D = b ** 2 - 4 * a * c
+    x_high = (2 * c) / (-b - math.sqrt(D))
+    # Might not be obvious, but why we take the largest root is found in report.
+    return x_high
+
+    # def set_edge_values(u,phi_x,phi_y,val):
+
+
+#     locs = np.where(u==val)
+#     phi_x[locs]=val
+#     phi_x[(locs[0],locs[1]+1)] = val
+#     phi_y[locs]=val
+#     phi_y[(locs[0]+1,locs[1])] = val
+#     # No return necessary
+
+def compute_distance_transform(u):
+    """
+    Compute the weighted distance transform with cost/image function u using a fast marching algorithm
+    We compute the distance transform on a staggered grid: this is more precise
+    :param u: nonnegative 2D array with cost in each cell/pixel, infinity is allowed.
+    :return: weighted distance transform
+
+    """
+    # nx,ny = u.shape
+
+    # Cost for moving along horizontal lines
+    u_x = np.ones([nx + 1, ny]) * np.inf
+    u_x[1:-1, :] = (u[1:, :] + u[:-1, :]) / 2
+    # Cost for moving along vertical lines
+    u_y = np.ones([nx, ny + 1]) * np.inf
+    u_y[:, 1:-1] = (u[:, 1:] + u[:, :-1]) / 2
+
+    # Initialize locations (known/unknown/exit/obstacle)
+    phi = np.ones_like(u) * np.inf
+    exit_locs = np.where(u == 0)
+    obstacle_locs = np.where(u == np.inf)
+    phi[exit_locs] = 0
+    all_cells = {(i, j) for i in range(nx) for j in range(ny)}
+    known_cells = {cell for cell in zip(exit_locs[0], exit_locs[1])}
+    unknown_cells = all_cells - known_cells - {cell for cell in zip(obstacle_locs[0], obstacle_locs[1])}
+    new_candidate_cells = get_new_candidate_cells(known_cells, unknown_cells)
+    candidate_cells = {cell: np.inf for cell in new_candidate_cells}
+    while unknown_cells:
+        for cell in new_candidate_cells:
+            potential = compute_potential(cell, [u_x, u_y], phi)
+            candidate_cells[cell] = potential
+        sorted_candidates = sorted(candidate_cells.items(), key=operator.itemgetter(1))
+        best_cell = sorted_candidates[0][0]
+        min_potential = candidate_cells.pop(best_cell)
+        phi[best_cell] = min_potential
+        unknown_cells.remove(best_cell)
+        known_cells.add(best_cell)
+        new_candidate_cells = get_new_candidate_cells({best_cell}, unknown_cells)
+
+    # While there are unknown cells:
+    # Compute the value of the candidate cells from the known cells
+    # Pick the cheapest candidate cell, make it known
+    # Get new candidate cells and continue while
+    return phi
+
+
+# Example:
+"""
+- - - -
+* * * -
+- * - -
+- - - -
+"""
+u = read_image()
+nx, ny = u.shape
+import matplotlib.pyplot as plt
+
+phi = compute_distance_transform(u)
+# plt.imshow(phi)
+# plt.show()
