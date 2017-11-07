e40782075e9787388faa59cedf6d8994b090526c
0mar <omsrichardson@gmail.com>
2017-11-07 23:29:20 +0100

    Module works correctly, but 40x slower than python version. Checks in progress

--- fortran/wdt_module.f90
+++ fortran/wdt_module.f90
@@ -1,9 +1,9 @@
 module wdt_module
     implicit none
-    public :: exists, new_candidate_cells, add_candidate_cells, propagate_dist
-    public :: n_x2,n_y2,obstacle_value,unknown_value
+    public :: exists, add_candidate_cells, propagate_dist
+    public :: nx,ny,obstacle_value,unknown_value
     public :: LEFT, DOWN, RIGHT, UP, NUM_DIRS, KNOWN, UNKNOWN, CANDIDATE, NEW_CANDIDATE
-    integer (kind=4) :: n_x2,n_y2,obstacle_value,unknown_value
+    integer (kind=4) :: nx,ny,obstacle_value,unknown_value
 
     integer (kind=4), parameter :: LEFT = 0
     integer (kind=4), parameter :: DOWN = 1
@@ -19,29 +19,29 @@ module wdt_module
 
 contains
 
-    function new_candidate_cells(cell_x,cell_y,wdt_field) result(cand_cells)
+!    function new_candidate_cells(cell_x,cell_y,wdt_field) result(cand_cells)
+!        implicit none
+!        ! Find new candidate cells
+!        integer (kind=4), intent(in) :: cell_x,cell_y
+!        real (kind=8), dimension(0:nx-1,0:ny-1), intent(in) :: wdt_field
+!        integer (kind=4), dimension(0:3,0:1) :: cand_cells !DON'T mark intent out
+!        integer (kind=4) :: direction,nb_x,nb_y
+!        cand_cells = -1
+!        do direction=0,3
+!            nb_x = sign(mod(direction+1,2),direction-1) + cell_x
+!            nb_y = sign(mod(direction,2),direction-2) + cell_y
+!            if (exists(nb_x,nb_y)==1)  then
+!                if (wdt_field(nb_x,nb_y) == unknown_value) then
+!                    cand_cells(direction,0) = nb_x
+!                    cand_cells(direction,1) = nb_y
+!                end if
+!            end if
+!        end do
+!    end function new_candidate_cells
+    subroutine add_candidate_cells(cell_x,cell_y, cell_indicators,nx,ny)
         implicit none
-        ! Find new candidate cells
-        integer (kind=4), intent(in) :: cell_x,cell_y
-        real (kind=8), dimension(0:n_x2-1,0:n_y2-1), intent(in) :: wdt_field
-        integer (kind=4), dimension(0:3,0:1) :: cand_cells !DON'T mark intent out
-        integer (kind=4) :: direction,nb_x,nb_y
-        cand_cells = -1
-        do direction=0,3
-            nb_x = sign(mod(direction+1,2),direction-1) + cell_x
-            nb_y = sign(mod(direction,2),direction-2) + cell_y
-            if (exists(nb_x,nb_y)==1)  then
-                if (wdt_field(nb_x,nb_y) == unknown_value) then
-                    cand_cells(direction,0) = nb_x
-                    cand_cells(direction,1) = nb_y
-                end if
-            end if
-        end do
-    end function new_candidate_cells
-    subroutine add_candidate_cells(cell_x,cell_y, cell_indicators)
-        implicit none
-        integer (kind=4) :: cell_x,cell_y
-        integer (kind=4), dimension(0:n_x2-1,0:n_y2-1) :: cell_indicators
+        integer (kind=4) :: cell_x,cell_y, nx,ny
+        integer (kind=4), dimension(0:nx-1,0:ny-1) :: cell_indicators
         integer (kind=4) :: direction
         integer (kind=4) :: nb_x,nb_y
 
@@ -56,10 +56,11 @@ contains
         end do
         end subroutine add_candidate_cells
 
-    subroutine propagate_dist(cell_x,cell_y,wdt_field,costs_x,costs_y,out_pot)
+    subroutine propagate_dist(cell_x,cell_y,wdt_field,costs_x,costs_y,nx,ny,out_pot)
     ! Todo: Can we make it faster by passing just the relevant array?
     !   Compute the potential in a single cell with a first order upwind method
     implicit none
+    integer (kind=4) :: cell_x,cell_y,nx,ny
     real (kind=8) :: a,b,c,D ! parameters for upwind approximation
     integer (kind=4) :: normal_x,normal_y,face_index_x,face_index_y! Administration
     integer (kind=4) :: nb_cell_x,nb_cell_y! neighbour indices
@@ -67,18 +68,17 @@ contains
 
     integer (kind=4) :: direction
 
-    real (kind=8), dimension(0:n_x2-1,0:n_y2-1) :: wdt_field
-    real (kind=8), dimension(0:n_x2,0:n_y2-1) :: costs_x
-    real (kind=8), dimension(0:n_x2-1,0:n_y2) :: costs_y
+    real (kind=8), dimension(0:nx-1,0:ny-1) :: wdt_field
+    real (kind=8), dimension(0:nx,0:ny-1) :: costs_x
+    real (kind=8), dimension(0:nx-1,0:ny) :: costs_y
     real (kind=8), dimension(0:3) :: neighbour_pots
-    integer (kind=4) :: cell_x,cell_y
     real (kind=8) :: pot, out_pot,cost
 
     neighbour_pots=  (/ obstacle_value, obstacle_value, obstacle_value, obstacle_value /) 
     hor_cost = obstacle_value
     ver_cost = obstacle_value
         ! Find the minimal directions along a grid cell.
-        ! Assume left and below are best, then over!write with right and up if they are better
+        ! Assume left and below are best, then overwrite with right and up if they are better
     do direction=0,3
             normal_x = sign(mod(direction+1,2),direction-1)
             normal_y = sign(mod(direction,2),direction-2)
@@ -158,7 +158,7 @@ contains
         ! Find out whether the cell exists given the dimensions of the scene
         integer (kind=4), intent(in) :: cell_x,cell_y
         integer (kind=4) :: ex
-        if (cell_x < 0 .or. cell_y < 0 .or. cell_x>=n_x2 .or. cell_y>=n_y2) then
+        if (cell_x < 0 .or. cell_y < 0 .or. cell_x>=nx .or. cell_y>=ny) then
             ex = 0
         else
             ex = 1
@@ -174,11 +174,11 @@ end module wdt_module
 
 program test_wdt
     implicit none
-    integer (kind=4), parameter :: n_x=4
-    integer (kind=4), parameter :: n_y=4
+    integer (kind=4), parameter :: n_x=900
+    integer (kind=4), parameter :: n_y=600
     integer (kind=4) :: i,j
     real (kind=8), dimension(0:n_x-1,0:n_y-1) :: cost_field,wdt_field
-    real (kind=8), parameter :: obstacle_value = 200
+    real (kind=8), parameter :: obstacle_value = 2000
 
     ! Example:
     !- - - -
@@ -186,19 +186,16 @@ program test_wdt
     !- * - -
     !- - - -
 
-    cost_field = 1
+    cost_field = 0.01
     cost_field(1,1) = 0
     cost_field(0:3,2) = 0
     cost_field(3,1:3)= obstacle_value
-    wdt_field = obstacle_value
-    wdt_field(2,2) = 0
+    cost_field(500,:) = obstacle_value
     call weighted_distance_transform(cost_field,wdt_field,n_x,n_y,obstacle_value)
-    do i=0,n_y-1
-        write(*,*) ( wdt_field(j,n_x-i-1), j=0,n_x-1 )
-    enddo
 end program
 
 subroutine weighted_distance_transform(cost_field,wdt_field,n_x,n_y,obs_val)
+    ! Todo: Somehow make sure I don't have to input the n_x/n_y
 use wdt_module
 implicit none
 !   Compute the potential in a single cell with a first order upwind method
@@ -221,8 +218,8 @@ integer (kind=4) :: cell_x,cell_y
 integer (kind=4) :: num_cand_cells
 real (kind=8) :: dist, min_dist
 
-n_x2 = n_x
-n_y2 = n_y
+nx = n_x
+ny = n_y
 obstacle_value = obs_val
 unknown_value = obs_val + 1
 ! Cost for moving along horizontal lines
@@ -236,15 +233,15 @@ costs_y(:,1:n_y-1) = (cost_field(:,1:n_y-1) + cost_field(:,0:n_y-2))/2
 !Initialize locations (known(exit/obstacles)/unknown)
 wdt_field = unknown_value !TODO: Faster to do in loop? Check
 cell_indicators = UNKNOWN ! Same!
-do i=0,n_x2-1
-    do j=0,n_y2-1
+do i=0,nx-1
+    do j=0,ny-1
         if (cost_field(i,j)==0) then
             ! No cost, so this is an exit
             wdt_field(i,j) = 0
             cell_indicators(i,j) = KNOWN
             ! ! Count the maximum number of candidate cells to allocate later
             ! num_cand_cells = num_cand_cells + NUM_DIRS
-            call add_candidate_cells(i,j,cell_indicators)
+            call add_candidate_cells(i,j,cell_indicators,nx,ny)
         elseif (cost_field(i,j)>=obstacle_value) then
             ! 'infinite cost', so this is an obstacle
             wdt_field(i,j) = obstacle_value
@@ -252,9 +249,9 @@ do i=0,n_x2-1
         endif
     enddo
 enddo
-    do i=0,n_y-1
-        write(*,*) ( cell_indicators(j,n_x-i-1), j=0,n_x-1 )
-    enddo
+!    do i=0,n_y-1
+!        write(*,*) ( cell_indicators(j,n_x-i-1), j=0,n_x-1 )
+!    enddo
 ! allocate(nb_values(0:num_cand_cells-1,0:1))
 
 ! nb_values = new_candidate_cells(2,1,wdt_field)
@@ -265,12 +262,12 @@ do while (.true.)
     min_dist = obstacle_value
     min_i = -1
     min_j = -1
-    do i=0,n_x2-1
-        do j=1,n_y2-1
+    do i=0,nx-1
+        do j=1,ny-1
             if (cell_indicators(i,j)>=CANDIDATE) then
                 ! Compute distances for all new candidate cells
                 if (cell_indicators(i,j) == NEW_CANDIDATE) then
-                    call propagate_dist(i,j,wdt_field,costs_x,costs_y,dist)
+                    call propagate_dist(i,j,wdt_field,costs_x,costs_y,nx,ny,dist)
                     if (dist < wdt_field(i,j)) then
                         ! If the distance is lower, overwrite
                         wdt_field(i,j) = dist
@@ -292,7 +289,7 @@ do while (.true.)
     else
     ! Mark the cell as known
         cell_indicators(min_i,min_j) = KNOWN
-        call add_candidate_cells(min_i,min_j,cell_indicators)
+        call add_candidate_cells(min_i,min_j,cell_indicators,nx,ny)
     end if
 end do
 
